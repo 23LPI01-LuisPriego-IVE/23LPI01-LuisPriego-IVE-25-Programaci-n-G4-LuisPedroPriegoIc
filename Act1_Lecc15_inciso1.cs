af5a454c23f54ffbea75113da7f7d60e9f7f27ce
Luis Pedro Priego Ic <lpriegodb23@gmail.com>
2026-05-10 00:23:01 -0600

    Add files via upload

---
+++ Act1_Lecc15_inciso1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SistemaCobroBoletos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total_boletos, total_dinero, tipo_boleto;
+
+            total_boletos = 0;
+            total_dinero = 0;
+            tipo_boleto = 1;
+
+            Console.WriteLine("Sistema de Cobro");
+            Console.WriteLine("1. Adulto ($15) | 2. Niño ($10) | 0. Cerrar Turno");
+
+            while (tipo_boleto != 0)
+            {
+                Console.WriteLine("Ingrese tipo de boleto:");
+                if (int.TryParse(Console.ReadLine(), out tipo_boleto))
+                {
+                    if (tipo_boleto == 1)
+                    {
+                        total_dinero = total_dinero + 15;
+                        total_boletos = total_boletos + 1;
+                    }
+                    else if (tipo_boleto == 2)
+                    {
+                        total_dinero = total_dinero + 10;
+                        total_boletos = total_boletos + 1;
+                    }
+
+                    if (tipo_boleto != 0)
+                    {
+                        Console.WriteLine("Ventas actuales: " + total_boletos + " boletos. Total: $" + total_dinero);
+                    }
+                }
+            }
+
+            Console.WriteLine("Corte Final");
+            Console.WriteLine("Boletos vendidos: " + total_boletos);
+            Console.WriteLine("Total de dinero cobrado: $" + total_dinero);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Act1_Lecc23_inciso1.psc
@@ -0,0 +1,17 @@
+Algoritmo Act1_Lecc23_inciso1
+	Definir numero_inicial, contador, resultado Como Entero
+	
+	Escribir "Dame un numero para mostrar su tabla de multiplicar (1 al 10):"
+	Leer numero_inicial
+	
+	contador = 1
+	
+	Escribir "Tabla del:", numero_inicial
+	
+	
+	Mientras contador <= 10 Hacer
+		resultado = numero_inicial * contador
+		Escribir numero_inicial, " x ", contador, " = ", resultado
+		contador = contador + 1
+	Fin Mientras
+FinAlgoritmo
